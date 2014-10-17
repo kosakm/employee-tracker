f35c6b96b4e0cd64c9695b265ea071d9256edc03
Austin <aluck@tomax.com>
2014-10-17 11:48:49 -0600

    Analytics page

--- public/javascripts/analytics.js
+++ public/javascripts/analytics.js
@@ -4,10 +4,39 @@ var outputDiv;
 var OUTPUT = "output";
 var STATUS = "status";
 var REQUEST = "REQUEST";
+var plot;
+var elecEmp;
 
 $(function () {
 	init();
 	testWebSocket();
+	
+	plot = $.plot("#Electronics", [ getRandomData(0,dataElecSales,'elecSales') ], {
+		series: {
+			shadowSize: 0	// Drawing is faster without shadows
+		},
+		yaxis: {
+			min: 0,
+			max: 100
+		},
+		xaxis: {
+			show: false
+		}
+	});
+	elecEmp = $.plot("#ElectronicsEmps", [ getRandomData(0,dataElecEmps,'elecEmps') ], {
+		series: {
+			shadowSize: 0	// Drawing is faster without shadows
+		},
+		yaxis: {
+			min: 0,
+			max: 6
+		},
+		xaxis: {
+			show: false
+		}
+	});
+
+	//update();
 });
 
 function init() {
@@ -35,7 +64,16 @@ function onClose(evt) {
 function onMessage(evt) { 
 	console.log("Received message");
 	console.log(evt);
-	writeToScreen("RECEIVED MESSAGE", OUTPUT); 
+	  var message;
+	  message = JSON.parse(evt.data);
+	  $("#footer").prepend(message);
+	  if (message.response.hasOwnProperty('associateId')) {
+		  elecEmp.setData([getRandomData(message.response.total,dataElecEmps, 'elecEmps')]);
+		  elecEmp.draw(); 
+	 } else if (message.response.hasOwnProperty('amount')) {
+		  plot.setData([getRandomData(message.response.amount,dataElecSales, 'elecSales')]);
+		  plot.draw();
+	 }
 }  
 
 
@@ -62,3 +100,44 @@ function writeToScreen(message, to) {
 	}
 	 
 }  
+
+var dataElecSales = [],
+dataElecEmps = [],
+totalPoints = 300;
+
+function getRandomData(amount,datag, type) {
+
+	if (datag.length > 0)
+		datag = datag.slice(1);
+	
+	// Do a random walk
+	
+	while (datag.length < totalPoints) {
+	
+		datag.push(amount);
+	}
+	
+	// Zip the generated y values with the x values
+	
+	var res = [];
+	for (var i = 0; i < datag.length; ++i) {
+		res.push([i, datag[i]])
+	}
+	switch (type) {
+		case "elecSales":
+			dataElecSales = datag;
+		case "elecEmp":
+			dataElecEmps = datag;
+	}
+	return res;
+}
+
+function update(amount) {
+
+	plot.setData([getRandomData(amount,dataElecEmps, 'elecEmps')]);
+
+	// Since the axes don't change, we don't need to call plot.setupGrid()
+
+	plot.draw();
+	//setTimeout(update, updateInterval);
+}
