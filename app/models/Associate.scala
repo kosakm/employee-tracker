c72157ec1cecb97eed4a8d6728a492c8827002f7
Matt Kosak <mkosak@tomax.com>
2014-10-17 11:48:38 -0600

    do it live

--- app/models/Associate.scala
+++ app/models/Associate.scala
@@ -1,7 +1,10 @@
 package models
 
+import play.api.libs.json._
+
 case class Associate(associateId: Int, firstName: String, lastName: String)
 
 object Associate {
-
+  implicit val associateReads: Reads[Associate] = Json.reads[Associate]
+  implicit val associateWrites: Writes[Associate] = Json.writes[Associate]
 }
--- app/models/AssociateLocation.scala
+++ app/models/AssociateLocation.scala
@@ -1,7 +1,16 @@
 package models
 
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.mvc.WebSocket.FrameFormatter
+import models._
+
 case class AssociateLocation(location: Location, associate: Associate)
 
 object AssociateLocation {
+  implicit val locationReads: Reads[AssociateLocation] = Json.reads[AssociateLocation]
+  implicit val locationWrites: Writes[AssociateLocation] = Json.writes[AssociateLocation]
 
+  implicit val locationFrame: Format[AssociateLocation] = Json.format[AssociateLocation]
+  implicit val locationFrameFormatter: FrameFormatter[AssociateLocation] = FrameFormatter.jsonFrame[AssociateLocation]
 }
